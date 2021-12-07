cd7ba18294b9bb84563b6e1a8620fd93a8cf1bfd
haoshuai999 <626593718@qq.com>
2021-12-07 13:19:25 -0500

    add bootstrap grid

--- src/App.js
+++ src/App.js
@@ -8,12 +8,46 @@ import WorldMap from './components/WorldMap/WorldMap';
 
 function App() {
   return (
-    <div>
-      <ColorArc />
-      <BarChart />
-      <ScatterChart />
-      <LineChart />
-      <WorldMap />
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+        <label for="pet-select">Choose a pet:</label>
+        <select id="pet-select">
+            <option value="">--Please choose an option--</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+        </select>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 text-center">
+          <ColorArc />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 text-center">
+          <BarChart />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 text-center">
+          <ScatterChart />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 text-center">
+          <LineChart />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 text-center">
+          <WorldMap />
+        </div>
+      </div>   
     </div>
   );
 }
