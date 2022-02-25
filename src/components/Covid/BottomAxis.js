d73a0e615a11594b2f1a80c45ff3db2e8771ece1
haoshuai999 <626593718@qq.com>
2022-02-25 11:11:45 -0500

    change axis ref by using D3 axis

--- src/components/Covid/BottomAxis.js
+++ src/components/Covid/BottomAxis.js
@@ -1,13 +1,29 @@
-export const BottomAxis = ({xScale, innerHeight, tickFormat, tickOffset = 3}) =>
-  xScale.ticks().map(tick => (
-    <g className="tick" key={tick} transform={`translate(${xScale(tick)},0)`}>
-      <line
-        y2={innerHeight}
-      />
-      <text 
-        style={{textAnchor: 'middle'}}
-        y={innerHeight + tickOffset}
-        dy="0.71em"
-      >{tickFormat(tick)}</text>
-    </g>
-  ))
+import React, { useRef, useEffect } from 'react';
+import { select, axisBottom } from 'd3';
+
+export const BottomAxis = ({ xScale, innerHeight, tickFormat, tickOffset = 3 }) =>
+  {
+    const ref = useRef(null);
+    useEffect(() => {
+      const xAxisG = select(ref.current);
+      const xAxis = axisBottom(xScale);
+      xAxisG
+        .call(xAxis.tickSize(-innerHeight).tickPadding([tickOffset]).tickFormat(tickFormat))
+        .call((g) => g.select(".domain").remove());
+    }, [])
+
+    return <g transform={`translate(0, ${innerHeight})`} ref={ref} />
+
+  };
+  // xScale.ticks().map(tick => (
+  //   <g className="tick" key={tick} transform={`translate(${xScale(tick)},0)`}>
+  //     <line
+  //       y2={innerHeight}
+  //     />
+  //     <text
+  //       style={{ textAnchor: 'middle' }}
+  //       y={innerHeight + tickOffset}
+  //       dy="0.71em"
+  //     >{tickFormat(tick)}</text>
+  //   </g>
+  // ))
